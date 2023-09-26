8b8a3067fa1b88c5d3a0bfa3faa71d8cc68890c5
ashish-1221 <maharanaashish72@gmail.com>
2023-09-27 02:44:00 +0530

    First Commit

---
+++ invoicely_vue/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1 class="title">Welcome to Invoicely</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'HomeView',
+  components: {
+
+  }
+}
+</script>
